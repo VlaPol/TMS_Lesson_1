6cc040cfcf9aafe1f151e2a64d1e0837269fddf1
pva <tango2002@gmail.com>
2022-12-23 12:39:31 +0300

    fix: calculates the age from the entered year of birth

--- src/LessonFirst.java
+++ src/LessonFirst.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class LessonFirst {
@@ -12,17 +13,31 @@ public class LessonFirst {
         System.out.print("Enter Your name: ");
         String inputName = scanner.nextLine();
 
-        System.out.print("Enter Your age: ");
-        int age = scanner.nextInt();
+        System.out.print("Enter your birth age: ");
+        int inputAge = scanner.nextInt();
 
         System.out.println("Thank you!");
 
-        if(age < 18){
-            System.out.println(inputName + ", in your "+ age + " you can't go to our club!!!");
-        }else {
-            System.out.println(inputName + ", WELCOME to our club!");
+        try{
+            int curAge = getAge(inputAge);
+            if(curAge < 18){
+                System.out.println(inputName + ", in your "+ curAge + " you can't go to our club!!!");
+            }else {
+                System.out.println(inputName + ", you have "+ curAge +" years, and you are WELCOME to our club!");
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
 
+    }
+
+    private static int getAge(int age) throws Exception{
+
+        if(age < LocalDate.now().getYear()) {
+            return LocalDate.now().getYear() - age;
+        }else{
+            throw new Exception("You are cheater!!!!");
+        }
     }
 
 }
